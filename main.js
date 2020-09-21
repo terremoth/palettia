5bdc4373d850186649fe56137ddf53496f053939
terremoth <dutr4@outlook.com>
2020-09-20 23:39:56 -0300

    testing library addtohomescreen.js

--- main.js
+++ main.js
@@ -115,50 +115,79 @@ colorPicker.addEventListener('blur', function(evt) {
 });
 
 
-var deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-// addBtn.style.display = 'none';
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    console.log("deferredPrompt:");
-    console.log(deferredPrompt);
-});
-    // Update UI to notify the user they can add to home screen
-
-addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
-    // addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-        } else {
-            console.log('User dismissed the A2HS prompt');
+// var deferredPrompt;
+// const addBtn = document.querySelector('.add-button');
+// // addBtn.style.display = 'none';
+//
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     // Prevent Chrome 67 and earlier from automatically showing the prompt
+//     e.preventDefault();
+//     // Stash the event so it can be triggered later.
+//     deferredPrompt = e;
+//     console.log("deferredPrompt:");
+//     console.log(deferredPrompt);
+// });
+//     // Update UI to notify the user they can add to home screen
+//
+// addBtn.addEventListener('click', () => {
+//     // hide our user interface that shows our A2HS button
+//     // addBtn.style.display = 'none';
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//         if (choiceResult.outcome === 'accepted') {
+//             console.log('User accepted the A2HS prompt');
+//         } else {
+//             console.log('User dismissed the A2HS prompt');
+//         }
+//         deferredPrompt = null;
+//     });
+// });
+//
+//
+// window.addEventListener('appinstalled', (evt) => {
+//     // Log install to analytics
+//     console.log('INSTALL: Success');
+// });
+//
+// if ('serviceWorker' in navigator) {
+//     // window.addEventListener('load', function() {
+//         navigator.serviceWorker.register('sw.js').then(function(registration) {
+//             // Registration was successful
+//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//         }, function(err) {
+//             // registration failed :(
+//             console.log('ServiceWorker registration failed: ', err);
+//         });
+//     // });
+// }
+( function () {
+    addToHomescreen({
+        autostart: true,
+        autoHide: 0,
+        logging: true,
+        minSessions: 2,
+        onShow: function () {
+            console.log("showing");
+        },
+        onInit: function () {
+            console.log("initializing");
+        },
+        onAdd: function () {
+            console.log("adding");
+        },
+        onInstall: function () {
+            console.log("Installing");
+        },
+        onCancel: function () {
+            console.log("Cancelling");
+        },
+        displayNextPrime: true,
+        customCriteria: true,
+        displayPace: 0,
+        customPrompt: {
+            src: "img/palettia-192x192.png"
         }
-        deferredPrompt = null;
     });
-});
-
-
-window.addEventListener('appinstalled', (evt) => {
-    // Log install to analytics
-    console.log('INSTALL: Success');
-});
-
-if ('serviceWorker' in navigator) {
-    // window.addEventListener('load', function() {
-        navigator.serviceWorker.register('sw.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    // });
-}
+})();
